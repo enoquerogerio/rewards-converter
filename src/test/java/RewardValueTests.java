46713ebb49d8e3e20dc11bc253bcc01a40d9e991
Enoque Rogério <enoquerogerio15mti25@gmail.com>
2024-09-12 23:28:00 +0100

    update test unit

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RewardValueTests {
@@ -20,11 +22,17 @@ public class RewardValueTests {
 
     @Test
     void convert_from_cash_to_miles() {
-        assert false;
+        double cashValue = 100;
+        int expectedResult = (int) (cashValue / RewardValue.MILES_TO_CASH_RATE);
+        var rewardValue = new RewardValue(cashValue);
+        assertThat(expectedResult, equalTo(rewardValue.getMilesValue()));
     }
 
     @Test
     void convert_from_miles_to_cash() {
-        assert false;
+        int milesValue = 28571;
+        double expectedResult = milesValue * RewardValue.MILES_TO_CASH_RATE;
+        var rewardValue = new RewardValue(milesValue);
+        assertThat(expectedResult, equalTo(rewardValue.getCashValue()));
     }
 }
