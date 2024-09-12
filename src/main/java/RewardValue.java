0aec11412ab1972fae57c46d3a973596050be136
Enoque Rogério <enoquerogerio15mti25@gmail.com>
2024-09-12 21:51:32 +0100

    Create a RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,27 +1,25 @@
 public class RewardValue {
     private static final double MILES_TO_CASH_RATE = 0.0035;
-    private double MilesValue;
+    private int MilesValue;
     private double CashValue;
 
     // Constructor for cash value
     public RewardValue(double cashValue) {
         this.CashValue = cashValue;
-        this.MilesValue = cashValue / MILES_TO_CASH_RATE;
+        this.MilesValue = (int) (cashValue / MILES_TO_CASH_RATE);
     }
 
     // Constructor for miles value
-    public RewardValue(double milesValue, boolean isMiles){
-        if(isMiles){
-            this.MilesValue = milesValue;
-            this.CashValue = milesValue * MILES_TO_CASH_RATE;
-        }
+    public RewardValue(int milesValue) {
+        this.MilesValue = milesValue;
+        this.CashValue = milesValue * MILES_TO_CASH_RATE;
     }
 
-    public double getMilesValue(){
+    public int getMilesValue() {
         return this.MilesValue;
     }
 
-    public double getCashValue(){
+    public double getCashValue() {
         return this.CashValue;
     }
 }
